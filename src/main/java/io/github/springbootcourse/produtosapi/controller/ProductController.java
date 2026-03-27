fc2dedc0838c2cf56e44a079fe454e32388f7bc1
Guilhermeewe <guilhermesarnaglia2@gmail.com>
2026-03-27 19:24:43 -0300

    Add endpoints, insertProduct, findAllProducts and findProductsById

--- src/main/java/io/github/springbootcourse/produtosapi/controller/ProductController.java
+++ src/main/java/io/github/springbootcourse/produtosapi/controller/ProductController.java
@@ -2,19 +2,43 @@ package io.github.springbootcourse.produtosapi.controller;
 
 
 import io.github.springbootcourse.produtosapi.model.Products;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.springbootcourse.produtosapi.repository.ProductRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("products")
 public class ProductController {
 
+    private ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
 
     @PostMapping
     public void insertProduct(@RequestBody Products product) {
+
+        var id = UUID.randomUUID().toString();
+        product.setId(id);
+        productRepository.save(product);
+
         System.out.println("Product insert " + product);
     }
+
+    @GetMapping("/all")
+    public List<Products> findAllProducts() {
+        return productRepository.findAll();
+
+    }
+
+    @GetMapping("{id}")
+    public Products findProductById(@PathVariable String id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
 
 }
